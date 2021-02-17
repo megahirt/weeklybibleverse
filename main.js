5329151caff254ae99c40dd4d9cb15274dd15b4a
Chris Hirt <chris@hirtfamily.net>
2021-02-17 20:31:43 +0700

    update data for year 2021

--- main.js
+++ main.js
@@ -156,127 +156,127 @@ const bible = {
     "Reference": "Luke 2:16-17",
     "Verse": "So they hurried off and found Mary and Joseph, and the baby, who was lying in the manger. When they had seen him, they spread the word concerning what had been told them about this child,"
   },
-  "2020-01-05": {
+  "2021-01-05": {
     "Holiday": "half week",
     "Lesson": "",
     "Reference": "",
     "Verse": ""
   },
-  "2020-01-11": {
+  "2021-01-11": {
     "Holiday": "",
     "Lesson": 19,
     "Reference": "1 Sam. 15:22-23",
     "Verse": "But Samuel replied:“Does the Lord delight in burnt offerings and sacrifices as much as in obeying the Lord? To obey is better than sacrifice, and to heed is better than the fat of rams. For rebellion is like the sin of divination, and arrogance like the evil of idolatry. Because you have rejected the word of the Lord, he has rejected you as king.”"
   },
-  "2020-01-18": {
+  "2021-01-18": {
     "Holiday": "",
     "Lesson": 20,
     "Reference": "Psalm 18:2",
     "Verse": "The Lord is my rock, my fortress and my deliverer; my God is my rock, in whom I take refuge, my shield and the horn of my salvation, my stronghold."
   },
-  "2020-01-25": {
+  "2021-01-25": {
     "Holiday": "",
     "Lesson": 21,
     "Reference": "Psalm 51:10",
     "Verse": "Create in me a pure heart, O God, and renew a steadfast spirit within me."
   },
-  "2020-02-01": {
+  "2021-02-01": {
     "Holiday": "",
     "Lesson": 22,
     "Reference": "Heb. 3:12",
     "Verse": "See to it, brothers and sisters, that none of you has a sinful, unbelieving heart that turns away from the living God."
   },
-  "2020-02-08": {
+  "2021-02-08": {
     "Holiday": "",
     "Lesson": 23,
     "Reference": "James 1:5",
     "Verse": "If any of you lacks wisdom, you should ask God, who gives generously to all without finding fault, and it will be given to you."
   },
-  "2020-02-15": {
+  "2021-02-15": {
     "Holiday": "",
     "Lesson": 24,
     "Reference": "1 Peter 3:8",
     "Verse": "Finally, all of you, be like-minded, be sympathetic, love one another,be compassionate and humble."
   },
-  "2020-02-22": {
+  "2021-02-22": {
     "Holiday": "",
     "Lesson": 25,
     "Reference": "Psalm 140:4",
     "Verse": "Keep me safe, Lord, from the hands of the wicked; protect me from the violent, who devise ways to trip my feet."
   },
-  "2020-03-01": {
+  "2021-03-01": {
     "Holiday": "",
     "Lesson": 26,
     "Reference": "1 Thess 5:12",
     "Verse": "Now we ask you, brothers and sisters, to acknowledge those who work hard among you, who care for you in the Lord and who admonish you."
   },
-  "2020-03-08": {
+  "2021-03-08": {
     "Holiday": "Spring Break",
     "Lesson": "",
     "Reference": "",
     "Verse": ""
   },
-  "2020-03-15": {
+  "2021-03-15": {
     "Holiday": "",
     "Lesson": 27,
     "Reference": "Isaiah 7:14",
     "Verse": "Therefore the Lord himself will give you a sign: The virgin will conceive and give birth to a son, and will call him Immanuel."
   },
-  "2020-03-22": {
+  "2021-03-22": {
     "Holiday": "",
     "Lesson": 28,
     "Reference": "Jer. 29:11",
     "Verse": "For I know the plans I have for you,” declares the Lord, “plans to prosper you and not to harm you, plans to give you hope and a future."
   },
-  "2020-03-29": {
+  "2021-03-29": {
     "Holiday": "",
     "Lesson": 30,
     "Reference": "Daniel 2:20",
     "Verse": "Praise be to the name of God for ever and ever; wisdom and power are his."
   },
-  "2020-04-05": {
+  "2021-04-05": {
     "Holiday": "Easter",
     "Lesson": 29,
     "Reference": "Isa 53:5",
     "Verse": "But he was pierced for our transgressions, he was crushed for our iniquities; the punishment that brought us peace was on him, and by his wounds we are healed."
   },
-  "2020-04-12": {
+  "2021-04-12": {
     "Holiday": "Songkran Break",
     "Lesson": "",
     "Reference": "",
     "Verse": ""
   },
-  "2020-04-19": {
+  "2021-04-19": {
     "Holiday": "",
     "Lesson": 31,
     "Reference": "John 4:23",
     "Verse": "Yet a time is coming and has now come when the true worshipers will worship the Father in the Spirit and in truth, for they are the kind of worshipers the Father seeks."
   },
-  "2020-04-26": {
+  "2021-04-26": {
     "Holiday": "",
     "Lesson": 32,
     "Reference": "Proverbs 19:21",
     "Verse": "Many are the plans in a person’s heart, but it is the Lord’s purpose that prevails."
   },
-  "2020-05-03": {
+  "2021-05-03": {
     "Holiday": "* 1/2 week",
     "Lesson": "",
     "Reference": "",
     "Verse": ""
   },
-  "2020-05-10": {
+  "2021-05-10": {
     "Holiday": "",
     "Lesson": 33,
     "Reference": "John 8:58",
     "Verse": "“Very truly I tell you,” Jesus answered, “before Abraham was born, I am!”"
   },
-  "2020-05-17": {
+  "2021-05-17": {
     "Holiday": "",
     "Lesson": 34,
     "Reference": "Rev. 1:3",
     "Verse": "Blessed is the one who reads aloud the words of this prophecy, and blessed are those who hear it and take to heart what is written in it,because the time is near."
   },
-  "2020-05-24": {
+  "2021-05-24": {
     "Holiday": "Last week",
     "Lesson": "",
     "Reference": "",
